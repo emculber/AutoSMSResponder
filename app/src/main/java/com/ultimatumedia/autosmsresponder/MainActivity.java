de753afc6523f2a1c1b4838fc16705b6e6266c47
Erik Culberson <eculberson12@gmail.com>
2015-04-22 21:02:45 -0400

    Search ability

--- app/src/main/java/com/ultimatumedia/autosmsresponder/MainActivity.java
+++ app/src/main/java/com/ultimatumedia/autosmsresponder/MainActivity.java
@@ -3,8 +3,13 @@ package com.ultimatumedia.autosmsresponder;
 import android.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,10 +20,32 @@ import com.ultimatumedia.autosmsresponder.ResponseTree.ResponseTree;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String[] COUNTRIES = new String[] { "Belgium",
+            "France", "France_", "Italy", "Germany", "Spain" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        // actionBar.setDisplayShowTitleEnabled(false);
+        // actionBar.setIcon(R.drawable.ic_action_search);
+
+        LayoutInflater inflator = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.actionbar, null);
+
+        actionBar.setCustomView(v);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView) v
+                .findViewById(R.id.editText1);
+        textView.setAdapter(adapter);
+
 
         ResponseTree responseTree = new ResponseTree();
         NumberListFragment numberListFragment = new NumberListFragment();
@@ -30,12 +57,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
         return true;
     }
 
